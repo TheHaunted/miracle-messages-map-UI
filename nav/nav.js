d2bbc21064f445d0343e2c28ec14c078f1dd26c6
TheHaunted <kevin_lopez@live.com>
2019-06-28 02:31:52 -0500

    reunion services expansion added to mobile

--- nav/nav.js
+++ nav/nav.js
@@ -1,4 +1,12 @@
+
+
 let menuIcon = document.querySelector('.icon');
 let mobileMenu = document.querySelector('.mobile-nav');
 
+let reunionService = document.querySelector('.reunion-service');
+let reunionMenu = document.querySelector('.reunion-service-items');
+console.log(reunionService);
+
 menuIcon.addEventListener('click', () => mobileMenu.classList.toggle('hidden'));
+
+reunionService.addEventListener('click', () => reunionMenu.classList.toggle('hidden'));
