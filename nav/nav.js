96db646b8d5aa18f45c6b04a3bc55ceb7c6b2b45
TheHaunted <kevin_lopez@live.com>
2019-06-28 02:37:21 -0500

    added about menu expansion

--- nav/nav.js
+++ nav/nav.js
@@ -5,8 +5,12 @@ let mobileMenu = document.querySelector('.mobile-nav');
 
 let reunionService = document.querySelector('.reunion-service');
 let reunionMenu = document.querySelector('.reunion-service-items');
-console.log(reunionService);
+
+let about = document.querySelector('.about');
+let aboutMenu = document.querySelector('.about-items');
 
 menuIcon.addEventListener('click', () => mobileMenu.classList.toggle('hidden'));
 
 reunionService.addEventListener('click', () => reunionMenu.classList.toggle('hidden'));
+
+about.addEventListener('click', () => aboutMenu.classList.toggle('hidden'));
